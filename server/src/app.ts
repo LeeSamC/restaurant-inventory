da4991d752b410b858f481a5385bad1e82bbf8e5
Lee <lee.samchong@gmail.com>
2026-08-26 15:03:38 -0400

    Express application

---
+++ server/src/app.ts
@@ -0,0 +1,30 @@
+import express from 'express'
+import cors from 'cors'
+import cookieParser from 'cookie-parser'
+
+import inventoryRouter from './modules/inventory/inventory.routes.js'
+import authRouter from './modules/auth/auth.routes.js'
+
+const app = express()
+
+app.use(
+    cors({
+        origin: process.env.CLIENT_URL,
+        credentials: true
+    })
+)
+
+app.use(express.json())
+app.use(cookieParser())
+
+app.get('/api/health', (req, res) => {
+    res.json({
+        success: true,
+        message: 'Restaurant Inventory API is running'
+    })
+})
+
+app.use('/api/auth', authRouter)
+app.use('/api/inventory', inventoryRouter)
+
+export default app
