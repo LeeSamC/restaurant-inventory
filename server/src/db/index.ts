f4131d9a3676e25d4106df1fa14a4c618b5b344d
Lee <lee.samchong@gmail.com>
2026-08-27 15:36:59 -0400

    added schema import so that I can use the tables

--- server/src/db/index.ts
+++ server/src/db/index.ts
@@ -1,6 +1,7 @@
 import 'dotenv/config'
 import {drizzle} from 'drizzle-orm/node-postgres'
 import pg from 'pg'
+import * as schema from './schema'
 
 const { Pool } = pg
 
@@ -12,4 +13,4 @@ export const pool = new Pool({
     connectionString: process.env.DATABASE_URL
 })
 
-export const db = drizzle(pool)
+export const db = drizzle(pool, {schema})
