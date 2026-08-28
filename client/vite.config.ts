a0ad855db633cdd61e07613a1adb7f9d7e000d23
Lee <lee.samchong@gmail.com>
2026-08-28 14:18:05 -0400

    PWA configurations

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,43 @@
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import {VitePWA} from 'vite-plugin-pwa'
+import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    tailwindcss(),
+
+    VitePWA({
+      registerType: 'autoUpdate',
+
+      manifest: {
+        name: 'Restaurant Inventory',
+        short_name: 'Inventory',
+        description: 'Restaurant inventory management system',
+        theme_color: '#111827',
+        background_color: 'f3f4f6',
+        display: 'standalone',
+
+        icons: [
+          {
+            src: '/pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      },
+
+      workbox: {
+        globPatterns: [
+          '**/*.{js,css,html,ico,png,svg}'
+        ]
+      }
+    })
+  ]
 })
