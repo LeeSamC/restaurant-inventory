29c7f4c118f561b62bf66b277319af78e4ac2c08
Lee <lee.samchong@gmail.com>
2026-08-29 15:08:46 -0400

    Added public and auth protected routes aslong with the login and registration routes

--- client/src/app/router.tsx
+++ client/src/app/router.tsx
@@ -3,19 +3,46 @@ import {createBrowserRouter} from 'react-router-dom'
 import AppLayout from '../layouts/AppLayout'
 import DashboardPage from '../pages/DashboardPage'
 import InventoryPage from '../modules/inventory/pages/InventoryPage'
+import LoginPage from '../modules/auth/pages/LoginPage'
+import RegistrationPage from '../modules/auth/pages/RegistrationPage'
+import ProtectedRoute from '../componenets/ProtectedRoute'
+import PublicRoute from '../componenets/PublicRoute'
+
 
 export const router = 
     createBrowserRouter([
         {
-            element: <AppLayout />,
+            element: <PublicRoute />,
             children: [
                 {
-                    path: '/',
-                    element: <DashboardPage />
+                    path: '/login',
+                    element: <LoginPage />
                 },
                 {
-                    path: '/inventory',
-                    element: <InventoryPage />
+                    path: '/register',
+                    element: <RegistrationPage />
+                }
+            ]
+        },
+
+        {
+            element: <ProtectedRoute />,
+
+            children: [
+                {
+                    element: <AppLayout />,
+
+                    children: [
+                        {
+                            path: '/',
+                            element: <DashboardPage />
+                        },
+
+                        {
+                            path: '/inventory',
+                            element: <InventoryPage />
+                        }
+                    ]
                 }
             ]
         }
